8fcbf9c059c1638cc1e067d0492542889e3aabf1
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-12-09 14:16:05 +0600

    project initiallized

---
+++ main.cs
@@ -0,0 +1,17 @@
+﻿// class MainProgram{
+//     public static void Main(string[] args){  
+//         int number = 10;
+//         long number2 = 1000000000000000000;
+//         float number3 = 10.5f;
+//         uint number4 = 10;
+
+//         Console.WriteLine(number,number2,number3,number4);
+//     }
+// }
+
+class MainProgram{
+    public static void Main(string[] args){
+        int number = 10;
+        Console.WriteLine(number);
+    }
+}
