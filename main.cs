ac0bf5d58b76449e3564947c90bedbc238d2ec9e
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-12-09 14:19:24 +0600

    mulitple intger print

--- main.cs
+++ main.cs
@@ -1,17 +1,11 @@
-﻿// class MainProgram{
-//     public static void Main(string[] args){  
-//         int number = 10;
-//         long number2 = 1000000000000000000;
-//         float number3 = 10.5f;
-//         uint number4 = 10;
-
-//         Console.WriteLine(number,number2,number3,number4);
-//     }
-// }
-
-class MainProgram{
-    public static void Main(string[] args){
+﻿class Ami{
+    public static void Main(string[] args){  
         int number = 10;
-        Console.WriteLine(number);
+        long number2 = 1000000000000000000;
+        float number3 = 10.5f;
+        uint number4 = 10;
+
+        // একাধিক মান প্রিন্ট করতে ফরম্যাট স্ট্রিং ব্যবহার করুন
+        Console.WriteLine("number:{0},number2:{1},number3:{2},number4:{3}",number,number2,number3,number4);
     }
 }
