e141c557b2d559dd75908d6eeeed36615478904f
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-12-09 20:52:01 +0600

    Convert any to number to any number and boolean

--- main.cs
+++ main.cs
@@ -48,7 +48,9 @@ class MainProgram{
     public static void Main(string[] args){
         decimal number = 114.34342434m;
         int number2 = Convert.ToInt32(number);  //convert deci to int (32 bit)
+        bool a = Convert.ToBoolean(number);
         Console.WriteLine(number2);
         Console.WriteLine(number2.GetType());
+        Console.WriteLine(a);
     }
 }
