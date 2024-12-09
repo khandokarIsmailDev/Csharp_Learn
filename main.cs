7e16bcd7a3335a5d60fab253953a7e41fcc0f6c6
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-12-09 15:28:07 +0600

    bool string char console

--- main.cs
+++ main.cs
@@ -1,11 +1,23 @@
-﻿class Ami{
-    public static void Main(string[] args){  
-        int number = 10;
-        long number2 = 1000000000000000000;
-        float number3 = 10.5f;
-        uint number4 = 10;
+﻿// class Ami{
+//     public static void Main(string[] args){  
+//         int number = 10;
+//         long number2 = 1000000000000000000;
+//         float number3 = 10.5f;
+//         uint number4 = 10;
 
-        // একাধিক মান প্রিন্ট করতে ফরম্যাট স্ট্রিং ব্যবহার করুন
-        Console.WriteLine("number:{0},number2:{1},number3:{2},number4:{3}",number,number2,number3,number4);
+//         // একাধিক মান প্রিন্ট করতে ফরম্যাট স্ট্রিং ব্যবহার করুন
+//         Console.WriteLine("number:{0},number2:{1},number3:{2},number4:{3}",number,number2,number3,number4);
+//     }
+// }
+
+class MainProgram{
+    public static void Main(string[] args){
+        bool a = true;
+        string str1 = "hello";
+        char c = 'a'; //single character
+        string str2 = str1 ;
+
+        Console.WriteLine("str1:{0},str2:{1},a:{2},c:{3}",str1,str2,a,c);
+
     }
 }
